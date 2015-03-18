6f80529c9356c432824c7ac223b5af217ff54cb3
Gianfranco Lagomarsino <gianlago@hotmail.com>
2015-03-18 16:21:20 -0400

    login_view.php
    actualize por segunda vez

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -8,7 +8,7 @@
 <<<<<<< HEAD
 <a href="<?php echo base_url('index.php/productos/administracion');   ?>">Administracion Productos Dollars del caso penta</a> |
 =======
-<a href="<?php echo base_url('index.php/productos/administracion');   ?>">Administracion Productos del tio jerry</a> |
+<a href="<?php echo base_url('index.php/productos/administracion');   ?>">Administracion Productos del tio jerr el alcoholicoy</a> |
 >>>>>>> origin/master
 <a href="<?php echo base_url('index.php/welcome/insertar');   ?>">Administracion Usuarios</a> |
 <a href="<?php echo base_url('index.php/welcome/modificar');   ?>">Administracion Clientes</a> |
