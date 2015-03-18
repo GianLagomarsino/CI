0000b46bcd955bc1df094b4aec81406285ea8151
Gianfranco Lagomarsino <gianlago@hotmail.com>
2015-03-18 17:28:42 -0300

    mensaje

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -6,9 +6,9 @@
 
 <nav>
 <<<<<<< HEAD
-<a href="<?php echo base_url('index.php/productos/administracion');   ?>">Administracion Productos Dollars del caso penta</a> |
+<a href="<?php echo base_url('index.php/productos/administracion');   ?>">Administracion Productos Dollars del caso penta, el embaucador</a> |
 =======
-<a href="<?php echo base_url('index.php/productos/administracion');   ?>">Administracion Productos del tio jerr el alcoholicoy</a> |
+<a href="<?php echo base_url('index.php/productos/administracion');   ?>">Administracion Productos del tio jerry el alcoholicoy</a> |
 >>>>>>> origin/master
 <a href="<?php echo base_url('index.php/welcome/insertar');   ?>">Administracion Usuarios</a> |
 <a href="<?php echo base_url('index.php/welcome/modificar');   ?>">Administracion Clientes</a> |
