bd8933bae95803f2ed1fdaf4eccdd823641c01a8
Sebastian <sebameruane@gmail.com>
2015-03-18 16:37:04 -0300

    sadasd
    sdad

---
+++ application/views/login_view.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head> </head>
+
+<body>
+
+<nav>
+<a href="<?php echo base_url('index.php/productos/administracion');   ?>">Administracion Productos</a> |
+<a href="<?php echo base_url('index.php/welcome/insertar');   ?>">Administracion Usuarios</a> |
+<a href="<?php echo base_url('index.php/welcome/modificar');   ?>">Administracion Clientes</a> |
+
+</nav>
+
+
+
+</form>
+
+</body>
+
+
+
+</html>
